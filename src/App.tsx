0771bdb3267e1d6fb5a2f0356048dc3d185d6d3f
Pablorbxm <pablouser1234@gmail.com>
2021-12-28 15:52:31 -0300

    github-api-project

--- src/App.tsx
+++ src/App.tsx
@@ -20,35 +20,35 @@ const Github: FC = ()=>{
 
     return <>
     <header className="h-and-f">
-            <h1 className="center w-text">GitHub API Project</h1>
-            <div className="flex-centralizado">
+            <h1 className="center-w-txt">GitHub API Project</h1>
+            <div className="center-objs">
                 <input type="text" onChange={(e)=>{setSearchName(e.target.value)}} placeholder="Digite o Nome Aqui" />
                 <button onClick={()=>{getGithubUser()}} type="button">Pesquisar</button>
             </div>
                 {notfound === true ? <h3 className="w-text center">Usúario não encontrado!</h3> : <></>}
     </header>
         <div className="div">
-                <h5 className="w-text center">Nome: {user.name}</h5>
-                <h5 className="w-text center">Nome de Usúario: {user.login}</h5>
+                <h5 className="center-w-txt">Nome: {user.name}</h5>
+                <h5 className="center-w-txt">Nome de Usúario: {user.login}</h5>
                 <img alt="" className="center-photo" src={user.avatar_url}/>
-                <h5 className="w-text center margin-text">Tipo: {user.type}</h5>
-                <h5 className="w-text center margin-text">Bio: {user.bio}</h5>
-                <h5 className="w-text center margin-textt">Companhia: {user.company}</h5>
-                <h5 className="w-text center margin-textt">Blog: {user.blog}</h5>
-                <h5 className="w-text center margin-text">ID: {user.id}</h5>
-                <h5 className="w-text center margin-text">E-mail: {user.email}</h5>
-                <h5 className="w-text center margin-text">Nome de Usúario do Twitter: {user.twitter_username}</h5>
-                <h5 className="w-text center margin-text">Seguidores: {user.followers}</h5>
-                <h5 className="w-text center margin-text">Seguindo: {user.following}</h5>
-                <h5 className="w-text center margin-text">Localização: {user.location}</h5>
-                <h5 className="w-text center margin-text">Criado em: {user.created_at}</h5>
-                <h5 className="w-text center margin-text">Atualizado em: {user.updated_at}</h5>
+                <h5 className="center-w-txt">Tipo: {user.type}</h5>
+                <h5 className="center-w-txt">Bio: {user.bio}</h5>
+                <h5 className="center-w-txt">Companhia: {user.company}</h5>
+                <h5 className="center-w-txt">Blog: {user.blog}</h5>
+                <h5 className="center-w-txtt">ID: {user.id}</h5>
+                <h5 className="center-w-txt">E-mail: {user.email}</h5>
+                <h5 className="center-w-txt">Nome de Usúario do Twitter: {user.twitter_username}</h5>
+                <h5 className="center-w-txt">Seguidores: {user.followers}</h5>
+                <h5 className="center-w-txt">Seguindo: {user.following}</h5>
+                <h5 className="center-w-txt">Localização: {user.location}</h5>
+                <h5 className="center-w-txt">Criado em: {user.created_at}</h5>
+                <h5 className="center-w-txt">Atualizado em: {user.updated_at}</h5>
         </div>
 
     <footer className="h-and-f">
-        <h5 className="center w-text">Este WebSite Foi Criado e Atualizado no Ano de 2021 Por Pablo Monteiro Da Silva.</h5>
-        <h5 className="center w-text">Este WebSite Mostra Apenas Informações Públicas Dos Usúarios do GitHub.</h5>
-        <h5 className="center w-text">Este Website Não Possui Copyright.</h5>
+        <h5 className="center-w-txt">Este WebSite Foi Criado e Atualizado no Ano de 2021 Por Pablo Monteiro Da Silva.</h5>
+        <h5 className="center-w-txt">Este WebSite Mostra Apenas Informações Públicas Dos Usúarios do GitHub.</h5>
+        <h5 className="center-w-txt">Este Website Não Possui Copyright.</h5>
     </footer>
     </>
 };
